4e9f7dcfc18990fe82b5cfed8a3f2b483235390f
Syed Abdul Munim Ali Shah <syedmuuni@gmail.com>
2024-11-01 00:52:04 +0500

    save changes

--- EcommerceWebsiteServies/ProductService.cs
+++ EcommerceWebsiteServies/ProductService.cs
@@ -22,34 +22,34 @@ namespace EcommerceWebsiteServies
         public async Task<IEnumerable<ProductDTO>> GetAllProduct()
         {
             var Produts = await _myContextDb.tbl_Product.Include(c => c.category).ToListAsync();
-            //Produts.Select(x => new ProductDTO { Id = x.Id, Name = x.Name, Description = x.Description,Price=x.Price,Image=x.Image,CategoryId=x.CategoryId,CategoryName=x.category.Name}).ToList();
+           return Produts.Select(x => new ProductDTO { Id = x.Id, Name = x.Name, Description = x.Description, Price = x.Price, Image = x.Image, CategoryId = x.CategoryId, CategoryName = x.category.Name }).ToList();
 
-            List<ProductDTO> productList = new List<ProductDTO>();
+            //List<ProductDTO> productList = new List<ProductDTO>();
 
-            foreach (var product in Produts)
-            {
-                ProductDTO ProductDto = new ProductDTO();
+            //foreach (var product in Produts)
+            //{
+            //    ProductDTO ProductDto = new ProductDTO();
 
-                ProductDto.Id = product.Id;
-                ProductDto.Name = product.Name;
-                ProductDto.Price = product.Price;
-                ProductDto.Description = product.Description;
-                ProductDto.CategoryId = product.CategoryId;
-                ProductDto.CategoryName = product.category.Name;
-                ProductDto.Image = product.Image;
+            //    ProductDto.Id = product.Id;
+            //    ProductDto.Name = product.Name;
+            //    ProductDto.Price = product.Price;
+            //    ProductDto.Description = product.Description;
+            //    ProductDto.CategoryId = product.CategoryId;
+            //    ProductDto.CategoryName = product.category.Name;
+            //    ProductDto.Image = product.Image;
              
 
 
-                productList.Add(ProductDto);
+            //    productList.Add(ProductDto);
 
 
-            }
-            return productList;
+            //}
+            //return productList;
 
 
         }
 
-        public async Task<Product> AddProduct(ProductDTOAdd productDTO)
+        public async Task<ProductDTOAdd> AddProduct(ProductDTOAdd productDTO)
         {
             try
             {
@@ -58,30 +58,35 @@ namespace EcommerceWebsiteServies
                     throw new KeyNotFoundException("This Product Is already addeed");
                 }
 
+                Product product = new Product();
 
-
-                productExist.Name = productDTO.Name;
-                productExist.Price = productDTO.Price;
-                productExist.Description = productDTO.Description;
-                productExist.CategoryId = productDTO.CategoryId;
+                product.Name = productDTO.Name;
+                product.Price = productDTO.Price;
+                product.Description = productDTO.Description;
+                product.CategoryId = productDTO.CategoryId;
 
                 string uniqueFileName=$"{Guid.NewGuid()}_{productDTO.UploadImage.FileName}";
                 string ImagePath = Path.Combine(_env.WebRootPath, "ProductImage", uniqueFileName);
                 Stream fs = new FileStream(ImagePath, FileMode.Create);
                 productDTO.UploadImage.CopyTo(fs);
 
-                productExist.Image = uniqueFileName;
+                product.Image = uniqueFileName;
 
 
-                await _myContextDb.tbl_Product.AddAsync(productExist);
+
+
+
+                await _myContextDb.tbl_Product.AddAsync(product);
                 await _myContextDb.SaveChangesAsync();
-                return productExist;
+                return productDTO;
             }
             catch (Exception)
             {
 
                 throw;
             }
+
+
         }
     }
 }
